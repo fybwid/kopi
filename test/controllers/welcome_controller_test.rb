fe149d3eda2c45dbd32008ebd2a2ebc2bde35a22
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-09-29 05:44:15 +0000

    Updated Welcome test - test Welcome as root page

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,11 +1,10 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get welcome_index_url
+  test "should get root" do
+    get root_path
     assert_response :success
     assert_select 'h1', "Welcome to Ruang Digital"
     assert_select 'p', "Ruang Kerja Tanpa Batas Dalam Bentuk Digital"
   end
-
 end
