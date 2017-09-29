a66db646cdaece72e6bbfcd6868ae5f0d24df432
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-09-29 05:04:47 +0000

    Updated Welcome page test - Test h1 element - Test p element

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -4,6 +4,8 @@ class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get welcome_index_url
     assert_response :success
+    assert_select 'h1', "Welcome to Ruang Digital"
+    assert_select 'p', "Ruang Kerja Tanpa Batas Dalam Bentuk Digital"
   end
 
 end
